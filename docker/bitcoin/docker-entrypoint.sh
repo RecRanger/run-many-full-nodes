aff686c86ccbe62e23ff2f6b2758af26d423110e
RecRanger <168371178+RecRanger@users.noreply.github.com>
2025-12-30 20:48:11 -0700

    Make Bitcoin docker config coin-agnostic

--- docker/bitcoin/docker-entrypoint.sh
+++ docker/bitcoin/docker-entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 if [ -n "${UID+x}" ] && [ "${UID}" != "0" ]; then
-  usermod -u "$UID" bitcoin
+  usermod -u "$UID" crypto_coin_user
 fi
 
 if [ -n "${GID+x}" ] && [ "${GID}" != "0" ]; then
-  groupmod -g "$GID" bitcoin
+  groupmod -g "$GID" crypto_coin_user
 fi
 
-echo "$0: assuming uid:gid for bitcoin:bitcoin of $(id -u bitcoin):$(id -g bitcoin)"
+echo "$0: assuming uid:gid for crypto_coin_user:crypto_coin_user of $(id -u crypto_coin_user):$(id -g crypto_coin_user)"
 
 if [ "$(echo "$1" | cut -c1)" = "-" ]; then
   echo "$0: assuming arguments for bitcoind"
@@ -18,21 +18,21 @@ if [ "$(echo "$1" | cut -c1)" = "-" ]; then
 fi
 
 if [ "$(echo "$1" | cut -c1)" = "-" ] || [ "$1" = "bitcoind" ]; then
-  mkdir -p "$BITCOIN_DATA"
-  chmod 700 "$BITCOIN_DATA"
+  mkdir -p "$CRYPTO_COIN_DATA"
+  chmod 700 "$CRYPTO_COIN_DATA"
   # Fix permissions for home dir.
-  chown -R bitcoin:bitcoin "$(getent passwd bitcoin | cut -d: -f6)"
+  chown -R crypto_coin_user:crypto_coin_user "$(getent passwd crypto_coin_user | cut -d: -f6)"
   # Fix permissions for bitcoin data dir.
-  chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+  chown -R crypto_coin_user:crypto_coin_user "$CRYPTO_COIN_DATA"
 
-  echo "$0: setting data directory to $BITCOIN_DATA"
+  echo "$0: setting data directory to $CRYPTO_COIN_DATA"
 
-  set -- "$@" -datadir="$BITCOIN_DATA"
+  set -- "$@" -datadir="$CRYPTO_COIN_DATA"
 fi
 
 if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
   echo
-  exec gosu bitcoin "$@"
+  exec gosu crypto_coin_user "$@"
 fi
 
 echo
